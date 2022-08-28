f5ae9856f738e5eadbe6a094ef63797bc69c7d01
Paul van Woensel <pvanwoensel@gmail.com>
2022-08-28 11:15:51 -0500

    basic adding and listing of beers

--- go.mod
+++ go.mod
@@ -2,13 +2,37 @@ module github.com/dutchrican/beer_api
 
 go 1.19
 
-require github.com/gofiber/fiber/v2 v2.36.0
+require (
+	github.com/gofiber/fiber/v2 v2.36.0
+	github.com/joho/godotenv v1.4.0
+)
 
 require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/cosmtrek/air v1.40.4 // indirect
+	github.com/creack/pty v1.1.11 // indirect
+	github.com/fatih/color v1.10.0 // indirect
+	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/georgysavva/scany v1.1.0 // indirect
+	github.com/go-pg/pg/v10 v10.10.6 // indirect
+	github.com/go-pg/zerochecker v0.2.0 // indirect
+	github.com/imdario/mergo v0.3.12 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/klauspost/compress v1.15.0 // indirect
+	github.com/lib/pq v1.10.6 // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/pelletier/go-toml v1.8.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/tmthrgd/go-hex v0.0.0-20190904060850-447a3041c3bc // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.38.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
+	github.com/vmihailenco/bufpool v0.1.11 // indirect
+	github.com/vmihailenco/msgpack/v5 v5.3.4 // indirect
+	github.com/vmihailenco/tagparser v0.1.2 // indirect
+	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
+	golang.org/x/crypto v0.0.0-20220214200702-86341886e292 // indirect
 	golang.org/x/sys v0.0.0-20220227234510-4e6760a101f9 // indirect
+	mellium.im/sasl v0.2.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,39 +1,119 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"os"
 
+	"github.com/georgysavva/scany/sqlscan"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/joho/godotenv"
+	"github.com/lib/pq"
+	_ "github.com/lib/pq"
 )
 
-func indexHandler(c *fiber.Ctx) error {
+type Beer struct {
+	ID             int     `json:"id"`
+	Beername       string  `db:"beer_name" json:"beer_name"`
+	Creator        string  `db:"creator" json:"creator"`
+	OriginCountry  string  `db:"origin_country" json:"origin_country"`
+	CurrentCountry string  `db:"current_country" json:"current_country"`
+	Alcohol        float32 `db:"alcohol" json:"alcohol"`
+}
+
+func indexHandler(c *fiber.Ctx, db *sql.DB) error {
+	var beers []Beer
+	err := sqlscan.Select(c.Context(), db, &beers, `SELECT * FROM public.beers`)
+
+	if err != nil {
+		log.Fatalln(err)
+		c.JSON("Error occurred")
+	}
+	return c.JSON(beers)
+}
+func postHandler(c *fiber.Ctx, db *sql.DB) error {
+	var b Beer
+	// var existingB Beer
+	if err := c.BodyParser(&b); err != nil {
+		return err
+	}
+	if len(b.Creator) == 0 || len(b.Beername) == 0 {
+		c.Context().SetStatusCode(fiber.StatusUnprocessableEntity)
+		return c.JSON(&fiber.Map{"error": "creator and beer_name must be provided"})
+	}
+	// err :=
+	stmt := `INSERT INTO public.beers (beer_name, creator, origin_country, current_country, alcohol) values ($1, $2, $3, $4, $5)`
+	_, err := db.Exec(stmt, b.Beername, b.Creator, b.OriginCountry, b.CurrentCountry, b.Alcohol)
+	if err != nil {
+		switch err.(*pq.Error).Code {
+		case "23502":
+			c.Context().SetStatusCode(fiber.StatusUnprocessableEntity)
+		default:
+			c.Context().SetStatusCode(fiber.StatusConflict)
+		}
+		return c.JSON(&fiber.Map{"error": err.(*pq.Error).Message})
+	}
+	c.Context().SetStatusCode(fiber.StatusCreated)
+	return c.JSON(b)
+}
+func putHandler(c *fiber.Ctx, db *sql.DB) error {
 	return c.SendString("Hi there!")
 }
-func postHandler(c *fiber.Ctx) error {
+func deleteHandler(c *fiber.Ctx, db *sql.DB) error {
 	return c.SendString("Hi there!")
 }
-func putHandler(c *fiber.Ctx) error {
-	return c.SendString("Hi there!")
-}
-func deleteHandler(c *fiber.Ctx) error {
-	return c.SendString("Hi there!")
+
+func getEnvVariables(key string) string {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+	return os.Getenv(key)
 }
 
 func main() {
 	app := fiber.New()
+	app.Use(logger.New())
+	port := getEnvVariables("PORT")
+	username := getEnvVariables("USERNAME")
+	password := getEnvVariables("PASSWORD")
+	db_port := getEnvVariables("DB_PORT")
+	db_ip := getEnvVariables("DB_IP")
+	connStr := fmt.Sprintf("postgresql://%s:%s@%s:%s/beers?sslmode=disable", username, password, db_ip, db_port)
 
-	app.Get("/", indexHandler)
-	app.Post("/", postHandler)
-	app.Put("/beer", putHandler)
-	app.Delete("/beer", deleteHandler)
-
-	port := os.Getenv("PORT")
-
-	if port == "" {
-		port = "3000"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	res, err := db.Exec(`CREATE TABLE IF NOT EXISTS beers (
+		id SERIAL PRIMARY KEY ,
+		beer_name TEXT NOT NULL UNIQUE,
+		creator TEXT NOT NULL,
+		origin_country TEXT,
+		current_country TEXT,
+		alcohol NUMERIC(4, 2)
+	 );`)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(res)
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return indexHandler(c, db)
+	})
+	app.Post("/", func(c *fiber.Ctx) error {
+		return postHandler(c, db)
+	})
+	app.Put("/beer", func(c *fiber.Ctx) error {
+		return putHandler(c, db)
+	})
+	app.Delete("/beer", func(c *fiber.Ctx) error {
+		return deleteHandler(c, db)
+	})
 
 	log.Fatalln(app.Listen(fmt.Sprintf(":%v", port)))
 }
